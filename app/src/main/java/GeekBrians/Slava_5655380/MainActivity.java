06cdf97e2b827e3a4623acaac81270b52ac636d8
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-03-27 00:24:24 +0300

    Создание проекта

---
+++ app/src/main/java/GeekBrians/Slava_5655380/MainActivity.java
@@ -0,0 +1,14 @@
+package GeekBrians.Slava_5655380;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
