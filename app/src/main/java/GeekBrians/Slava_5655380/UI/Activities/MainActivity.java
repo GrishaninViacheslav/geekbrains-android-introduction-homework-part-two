95a5c2d890afd8f279cde92714723b5226f4f2fb
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-03-27 04:13:05 +0300

    Удаление лже MainActivity беспричинно добавленного студией

--- app/src/main/java/GeekBrians/Slava_5655380/UI/Activities/MainActivity.java
+++ app/src/main/java/GeekBrians/Slava_5655380/UI/Activities/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.app_bar_main);
+        setContentView(R.layout.activity_main);
         initToolbar();
     }
 
